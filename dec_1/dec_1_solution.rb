7d8dc7df21a6100c5b3ae00cfbf0701dd992788d
parhaml <lee.p.parham@gmail.com>
2022-12-05 11:46:12 -0600

    Cleanup Day 1

--- dec_1/dec_1_solution.rb
+++ dec_1/dec_1_solution.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
-# read in file cole golf :|
-require 'benchmark'
+cal_counts = File
+             .read('calorie_input.txt')
+             .gsub(/\n{1}/, ',').split(',,')
+             .map { |group| group.split(',').map(&:to_i).sum }
 
-Benchmark.bm do |x|
-  x.report do
-    cal_counts = File
-                 .read('calorie_input.txt')
-                 .gsub(/\n{1}/, ',').split(',,')
-                 .map { |group| group.split(',').map(&:to_i).sum }
+puts "Single Elf max: #{cal_counts.max}"
 
-    puts "Single Elf max: #{cal_counts.max}"
-
-    # three elfs worth
-
-    puts "Three elf max: #{cal_counts.max(3).sum}"
-  end
-end
+puts "Three elf max: #{cal_counts.max(3).sum}"
